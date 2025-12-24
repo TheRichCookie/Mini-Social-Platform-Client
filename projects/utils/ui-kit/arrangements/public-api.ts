a6533c07204691b967f408f16826b79ee2adef02
AmirHossein Salehi <a.salehi@baman.club>
2025-12-24 18:55:20 +0330

    client update

--- projects/utils/ui-kit/arrangements/public-api.ts
+++ projects/utils/ui-kit/arrangements/public-api.ts
@@ -1,4 +1,4 @@
-// page
+//
 export * from './page/inner-page-wrapper/inner-page-wrapper.component';
 export * from './page/page.component';
 export * from './page/page-body/page-body.component';
@@ -6,15 +6,7 @@ export * from './page/page-footer/page-footer.component';
 export * from './page/page-header/page-header.component';
 export * from './page/page-part/page-part.component';
 
-// desktop-form
-export * from './desktop-form/desktop-form.component';
-export * from './desktop-form/desktop-form-body/desktop-form-body.component';
-export * from './desktop-form/desktop-form-body/desktop-form-part/desktop-form-part.component';
-export * from './desktop-form/desktop-form-body/desktop-form-part/desktop-form-row/desktop-form-controller/desktop-form-controller.component';
-export * from './desktop-form/desktop-form-body/desktop-form-part/desktop-form-row/desktop-form-controller/desktop-form-errors/desktop-form-errors.component';
-export * from './desktop-form/desktop-form-body/desktop-form-part/desktop-form-row/desktop-form-row.component';
-
-// form
+//
 export * from './form/form.component';
 export * from './form/form-body/form-body.component';
 export * from './form/form-body/form-part/form-array/form-array.component';
@@ -29,13 +21,9 @@ export * from './form/form-body/form-part/form-row/form-row.component';
 export * from './form/form-footer/form-footer.component';
 export * from './form/form-header/form-header.component';
 
-// list
+//
 export * from './list/list-items/list-item-wrapper/list-item-wrapper.component';
 export * from './list/list-items/list-items.component';
-
-// choice
-export * from './choice/choice-items/choice-item/choice-item.component';
-export * from './choice/choice-items/choice-items.component';
 
 //
 export * from './scroll/scroll.component';
@@ -46,13 +34,6 @@ export * from './button-group/button-group.component';
 //
 export * from './modal-frame/modal-frame.component';
 
-// cards
-export * from './card/basic-card/basic-card.component';
-
-// multi-tab
+//
 export * from './multi-tab/multi-tab-panel/multi-tab-panel.component';
 export * from './multi-tab/multi-tab-view/multi-tab-view.component';
-
-// desktop-multi-tab
-export * from './desktop-multi-tab/desktop-multi-tab-panel/desktop-multi-tab-panel.component';
-export * from './desktop-multi-tab/desktop-multi-tab-view/desktop-multi-tab-view.component';
